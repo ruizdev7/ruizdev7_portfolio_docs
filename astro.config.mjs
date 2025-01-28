186f3f503680ac8068dbddc9b133e35b4d3c30cf
Joseph Ruiz <ruizdev7@outlook.com>
2025-01-28 18:19:24 +0100

    Update README.md and add untracked files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,25 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "My Docs",
+      title: "ruizdev7 Docs",
       social: {
-        github: "https://github.com/withastro/starlight",
+        github: "https://github.com/ruizdev7",
       },
       sidebar: [
         {
-          label: "Guides",
+          label: "Como instalar Docker",
           items: [
             // Each item here is one entry in the navigation menu.
             {
-              label: "Example Guide",
+              label: "Example Guide how to install docker on Ubuntu",
+              slug: "guides/how_implement_docker",
+            },
+            {
+              label: "Example Guide how to install docker on Ubuntu",
+              slug: "guides/how_implement_docker",
+            },
+            {
+              label: "Example Guide how to install docker on Ubuntu",
               slug: "guides/how_implement_docker",
             },
           ],
